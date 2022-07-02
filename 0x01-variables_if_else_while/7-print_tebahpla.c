d443e785fd9ee13db78d8e74250c41360faa3c2c
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-01 20:03:25 -0700

    Added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter = 'z';
+	/* betty style doc for function main goes there */
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		last--;
+	}
+	return (0);
+}
