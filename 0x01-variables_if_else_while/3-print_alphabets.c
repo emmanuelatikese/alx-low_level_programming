712b3895e14cee6b1c9802fadcd43bf3faf9f0b9
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-02 07:23:39 -0700

    Added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+	/* betty style doc for function main goes there */
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter;
+	}
+	letter = 'A';
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	return (0);
+}
