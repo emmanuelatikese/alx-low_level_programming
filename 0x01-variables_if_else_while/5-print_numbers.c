38f75a3e37e4a0cfffd51e46d05337fb50ee5c4c
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-01 18:32:25 -0700

    Added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 0;
+	/* betty style doc for function main goes there */
+	for (num; num < 10; num++)
+	{
+		printf("%d");
+	}
+	return (0);
+}
