0e943f115e916b42eb76a46ef49a5f0c514dfa0a
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-03 16:23:08 -0700

    Added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int first, last;
+	/* betty style doc for function main goes there */
+	for (first = 0; first < 9; first++)
+	{
+		if (first == 7 && last < 8)
+		{
+			continue;
+		}
+		for (last = 1; last < 10; last++)
+		{
+			putchar((first % 10) + '0');
+			putchar((last % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	return (0);
+}
