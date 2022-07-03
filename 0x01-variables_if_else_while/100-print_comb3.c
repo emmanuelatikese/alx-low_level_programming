6e1e2ab261087f6e317fc20cf4e2ecf8185a2fcb
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-03 16:54:33 -0700

    Added

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,12 @@ int main(void)
 	/* betty style doc for function main goes there */
 	for (first = 0; first < 9; first++)
 	{
-		if (first == 5 && last < 6)
-		{
-			continue;
-		}
-		if (first == 7 && last < 8)
-		{
-			continue;
-		}
 		for (last = 1; last < 10; last++)
 		{
+			if (first == 5 && last < 6)
+				continue;
+			if (first == 8 && last < 9)
+				continue;
 			putchar((first % 10) + '0');
 			putchar((last % 10) + '0');
 			putchar(',');
