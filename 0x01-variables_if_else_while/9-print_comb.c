a0fb71e91e927e0db7fab547f15fed81726a7811
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-04 00:39:12 -0700

    Added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	/* betty style doc for function main goes there */
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+			putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
