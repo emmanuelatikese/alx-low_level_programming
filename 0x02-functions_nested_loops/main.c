d3540685ed8ad0ff9f2e119364be468944b04724
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-05 14:19:44 -0700

    Added

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,10 +1,19 @@
 #include "main.h"
 #include "1-alphabet.c"
 #include "2-print_alphabet_x10.c"
+#include "3-islower.c"
 #include "_putchar.c"
 
 int main(void)
 {
-	print_alphabet_x10();
-	return (0);
+	int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
