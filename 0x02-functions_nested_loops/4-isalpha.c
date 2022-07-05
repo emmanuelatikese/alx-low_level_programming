ff528d4f1bbf2f9e638651c2e5e4bf1a4bbd8842
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-05 14:41:41 -0700

    Added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ *@c: input c is a character
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 60 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -3,16 +3,19 @@
 #include "2-print_alphabet_x10.c"
 #include "3-islower.c"
 #include "_putchar.c"
+#include "4-isalpha.c"
 
 int main(void)
 {
-	int r;
+	 int r;
 
-    r = _islower('H');
+    r = _isalpha('H');
     _putchar(r + '0');
-    r = _islower('o');
+    r = _isalpha('o');
     _putchar(r + '0');
-    r = _islower(108);
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
