9cc485c2171d5219e4c6d6c83eb2e9b0fb732c5c
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-05 08:38:56 -0700

    Added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* betty style doc forfunctionmaingoes there */
+	printf("_putchar\n");
+	return (0);
+}
