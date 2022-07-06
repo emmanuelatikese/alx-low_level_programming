2b328c94bac4e2231a9750c6f184baf44cdd8d5a
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-06 15:44:36 -0700

    Added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 1, b;
+
+	while (n < 1024)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			b += n;
+		}
+		n++;
+	}
+	printf("%d", b);
+	printf("\n");
+}
