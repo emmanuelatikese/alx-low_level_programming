74122dfd15246c1b0583c6695c0cbfdfe542fc3d
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-08 03:25:50 -0700

    Add

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,18 @@ void more_numbers(void)
 
 	for (count = 1; count < 11; count++)
 	{
-		for (n = 1; n < 15; n++)
+		for (n = 0; n < 15; n++)
 		{
 			if (n < 10)
+			{	
 				_putchar(n + '0');
+			}
 			else if (n > 9)
+			{	
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
+		
+			}
 		}
 		_putchar('\n');
 	}
