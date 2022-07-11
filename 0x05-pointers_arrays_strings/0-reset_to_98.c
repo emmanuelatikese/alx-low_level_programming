685115c592de3ac7f7d73058741a75d50f3ab439
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-11 03:08:06 -0700

    Add

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ *@n: input n is an integer pointer
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	int a;
+
+	*n = 98;
+	n = &a;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef _MAIN__H__
+#define _MAIN__H__
+#include <stdio.h>
+void reset_to_98(int *n);
+#endif
