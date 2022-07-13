75c8c525c22a8b5f5543fed8eaeb7f7cddb7cc51
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-12 17:37:45 -0700

    add

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - Entry point
+ *@a: input a is the first argument
+ *@n: input n is the second argument
+ * Return: Always 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+}
