ee79baf1d2f7a4aa25b50077b13bb2be2138b5e7
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-13 19:29:07 -0700

    add

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,6 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
+	strncpy(dest, src, n);
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef _MAIN__H__
 #define _MAIN__H__
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
