6218334cdbc44e09c8d499cc99b6cc1f1b2b4734
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-18 17:22:33 -0700

    add

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _memcpy - Entry point
+ *@dest: input dest is the first argument
+ *@src: input src is the second argument
+ *@n: input n is the third argument
+ * Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a = 0;
+
+	while ( a < n)
+	{
+		*(dest + a) = *(src + a);
+	}
+	return (dest);
+}
