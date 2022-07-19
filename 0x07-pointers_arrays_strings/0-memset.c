f457dc039d24f086d1b8d2b220749323288de318
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-18 17:09:19 -0700

    add

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,13 +4,13 @@
  *@s: input s is the first argument
  *@b: input b is the second argument
  *@n: input n is the third argument
- * Return: Always string
+ * Return: string
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
 
-	while (a < n)
+	for (a = 0; a < n; )
 	{
 		s[a] = b;
 		a++;
