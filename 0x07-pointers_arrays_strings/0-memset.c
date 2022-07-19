f6d2e9a6223a7d205f378c8bce8e765238620914
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-18 17:16:42 -0700

    add

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,4 +15,5 @@ char *_memset(char *s, char b, unsigned int n)
 		*(s + a) = b;
 		a++;
 	}
+	return (s);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef _MAIN__H__
 #define _MAIN_H__
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 #endif
