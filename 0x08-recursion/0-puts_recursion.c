07a6e1bf6f57657ca210f7c65227fb3d84599821
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-20 17:42:49 -0700

    add

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - Entry point
+ *@s: input s is a parameter
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[i]);
+	_put_recursion(s + 1);
+}
