abf3eec047a108170551cd5e1458c926e2f4b8bf
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-22 18:38:33 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *@argc: input argc is the first argument
+ *@argv: input argv is the second argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, s, result = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (s = 0; argv[i][s]; s++)
+		{
+			if (argv[i][s] < '0' || argv[i][s] > '9')
+			{
+				prinft("Error\n");
+				return (1);
+			}
+		}
+		result = result + atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
