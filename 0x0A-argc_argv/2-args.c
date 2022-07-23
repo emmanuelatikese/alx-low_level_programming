585adb85e30c7a6a5bc44b9052e6cb6aacc4c3ab
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-22 18:09:23 -0700

    Add

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *@argc: input argc is the first argument
+ *@argv: input argv is the second argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%d\n", *argv);
+	return (0);
+}
