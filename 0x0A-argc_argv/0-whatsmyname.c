5c38a6d48e9fc0c3f3da52c04762126eb6afd5ef
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-22 17:47:32 -0700

    add

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  *@argv: input argv is the second argument
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__))argc, char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
