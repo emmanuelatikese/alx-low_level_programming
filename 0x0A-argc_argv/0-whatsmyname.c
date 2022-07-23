f476997eb247c66425dcd81ebb03e82ebeb6962a
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-07-22 17:36:28 -0700

    add

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *@argc: input argc is the first argument
+ *@argv: input argv is the second argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,4 @@
+#ifndef _MAIN__H__
+#define _MAIN__H__
+#include <stdio.h>
+#endif
