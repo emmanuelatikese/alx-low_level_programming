01c4ff412d98e8cf77aced44c3a9923ba5982f11
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-08-04 12:19:21 -0700

    add

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,6 +2,7 @@
 /**
  *sum_them_all - Entry point
  *@n: input n is the first argument
+ *@...: showing lot of argument can be used
  * Return: Always 0
  */
 int sum_them_all(const unsigned int n, ...)
