705b443ff2900e57e8a50c9608ec49dad85360ae
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-08-04 11:54:07 -0700

    add

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *sum_them_all - Entry point
+ *@n: input n is the first argument
+ * Return: Always 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list p;
+
+	int i, result = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(p, int);
+	va_end(p);
+	return (result);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_numbers - Entry point
+ *@separator: input separator is the first argument
+ *@n: input n is the second argument
+ * Return: Always 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list a;
+
+	int i, result;
+
+	va_start(a, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(a, int);
+		if (i != n - 1 && *separator != NULL)
+			printf("%s", *separator);
+	}
+	va_end(a);
+}
