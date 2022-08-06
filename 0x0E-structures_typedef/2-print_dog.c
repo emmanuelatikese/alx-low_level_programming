a5845df8ca67ee08a9f42072200e14d744386cd7
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-08-06 04:15:18 -0700

    add

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+/**
+ *print_dog - Entry point
+ *@d: input d is the only argument
+ * Return: Always 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf(" ");
+	if ((*d).name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", (*d).name);
+	if ((*d).age == 0)
+		printf("Age: (nil)");
+	else
+		printf("Age: %f\n", (*d).age);
+	if ((*d).owner == NULL)
+		printf("owner: (nil)");
+	else
+		printf("Owner: %s", (*d).owner);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#ifndef _DOG__H__
+#define _DOG__H__
+#include <stdio.h>
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
