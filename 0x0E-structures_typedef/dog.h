4ec92c4d2c0366b5697391d86f7cf5446f1a7b99
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-08-06 04:45:17 -0700

    add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
 #ifndef _DOG__H__
 #define _DOG__H__
 #include <stdio.h>
+/**
+ *dog - Entry point
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+/**
+ *dog_t - Entry point
+ */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
