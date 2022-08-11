ad79de50ecc2685f9e17a7173539a3f45bb4e3e0
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-08-11 05:43:59 -0700

    add

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,7 +13,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	list_t *n, *l;
 
-	n = malloc(sizeof(list_t);
+	n = malloc(sizeof(list_t));
 	if (n == NULL)
 		return (NULL);
 	d = strdup(str);
