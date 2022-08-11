74ac4928f25c0bf2885350c79ea46b2f972fdde8
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-08-11 11:36:31 -0700

    add

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *free_list - Entry point
+ * Return: Always 0
+ */
+void __attribute__((constructor)) first_comment(void);
+void first_comment(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n")
+}
