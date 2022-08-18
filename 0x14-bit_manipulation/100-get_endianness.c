34ce61a96a12051db7bc5f9a65c5b4dead86db06
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-08-18 15:09:49 -0700

    add

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *get_endianness - Entry point
+ * Return: Always 0
+ */
+int get_endianness(void)
+{
+	int n = 1;
+	
+	char *d;
+	
+	d = (char *)&n;
+	if (*d == 1)
+		return (1);
+	return (0);
+}
