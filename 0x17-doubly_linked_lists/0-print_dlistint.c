746edf059a9b6380f2a16736e2d769ba931cd7ab
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-14 05:02:00 -0700

    adding

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,12 +4,12 @@
 #include "lists.h"
 
 /**
- * print_dlist - this prints nodes
+ * print_dlistint - this prints and returns the number of nodes
  * @h: this is a node head.
- * Return: the number element in the linkedlist
  *
- * description - this prints and returns the number of nodes
+ * Return: the number element in the linkedlist
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	dlistint_t *ptr = head;
