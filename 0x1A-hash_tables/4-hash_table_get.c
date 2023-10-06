3c3eb504f80f4330aecfcf8e9db202dd386fd5be
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-06 16:29:43 -0700

    adding

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -26,7 +26,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(new->key, key) == 0)
 			return new->value;
-		node = node->next;
+		new = new->next;
 	}
 	return (NULL);
 }
