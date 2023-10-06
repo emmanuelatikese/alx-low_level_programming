facdee754c16cc91b7d112cc58dd773f8c588784
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-06 02:21:35 -0700

    adding

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,7 @@
 #ifndef HAST_TABLE_H
 #define HASH_TABLE_H
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,9 +16,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -31,8 +32,8 @@ typedef struct hash_node_s
 
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
