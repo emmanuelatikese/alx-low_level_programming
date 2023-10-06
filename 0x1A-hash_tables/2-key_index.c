4067a6404c96dda3780a65711cdab1950976c564
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-06 10:11:38 -0700

    adding

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_table.h"
+
+/**
+ * key_index - this is create a node and others
+ * @key: this is a char character
+ * @size: a long unsigned int
+ * Return: return non negative value
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int result = hash_djb2(key) % size;
+
+	return (result);
+}
