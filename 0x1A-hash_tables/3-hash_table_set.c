ade5628dbdfae734900f16e2fef6be3bc0c0905a
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-06 14:34:40 -0700

    adding

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,6 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	unsigned long int x, i;
+
 	x = key_index((const unsigned char *)key, ht->size);
 	i = x;
 
