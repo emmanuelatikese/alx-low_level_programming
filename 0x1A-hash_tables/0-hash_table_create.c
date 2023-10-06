3e505a2a3124045ab517eb96050b4382828a1701
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-06 01:51:55 -0700

    adding

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,5 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!new)
 		return (NULL);
 	new->size = size;
+	new->array = malloc(sizeof(hash_node_t) * size);
+	if (!new->array)
+		return (NULL);
+
+	int i = 0;
+
+	while (i < size)
+	{
+		new->array[i] = NULL;
+		i++;
+	}
 	return (new);
 }
