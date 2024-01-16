a174477cf2c8f67d621feef742a7c2ca26d6fa6a
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-16 07:27:49 -0800

    adding

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+/**
+ * binary_search - This is a binary algo function
+ * @array: list of arrays of ints
+ * @size: the length of the array
+ * @value: the int query
+ *
+ * Return: returns -1 or int
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int lo = 0, hi = (int) size;
+	int mid, i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (; lo < hi; lo++)
+	{
+		mid = (lo + hi) / 2;
+
+		printf("Searching in array: ");
+		for (i = lo; i < (hi - 1); i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[hi - 1]);
+
+		if (array[mid] > value)
+			hi = mid - 1;
+		else if (array[mid] < value)
+			lo = mid + 1;
+		else
+			return (mid);
+	}
+	return (-1);
+}
