6ac20eb8e37ae3a72c11459392af31978730c0bc
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-16 07:50:33 -0800

    adding

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -23,7 +23,7 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = lo; i < (hi - 1); i++)
 			printf("%d, ", array[i]);
-		printf("%d\n", array[hi - 1]);
+		printf("%d\n", array[i]);
 
 		if (array[mid] > value)
 			hi = mid - 1;
