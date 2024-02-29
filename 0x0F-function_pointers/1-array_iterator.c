1db6b003527bf4571d3b7f2ec372a6f70752b5d0
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-02-29 03:00:29 -0800

    adding file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
 
-	if (array == NULL || action == NULL || size == NULL)
+	if (array == NULL || action == NULL || !(size))
 		return;
 	for (i = 0; i < (int) size; i++)
 	{
