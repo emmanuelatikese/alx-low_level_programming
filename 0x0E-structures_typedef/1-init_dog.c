68d3c70a8abbe345a6e6b50b89292cb07d0867f7
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-12 10:24:31 -0700

    adding

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,17 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
- *init_dog - Entry point
- *@d: input d is the first argument
- *@name: input name is the second argument
- *@age: input age is the third argument
- *@owner: input owner is the last argument of the function
- * Return: Always 0
+ * init_dog - initial dog
+ * @d: struct
+ * @name: string
+ * @age: float
+ * @owner: string
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
