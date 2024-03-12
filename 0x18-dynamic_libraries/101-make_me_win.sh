413944778ed7fa8e79777920f456e9fecd2f2c43
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-12 09:52:50 -0700

    adding

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/emmanuelatikese/alx-low_level_programming/blob/main/0x18-dynamic_libraries/hk.so
+wget -P .. https://raw.githubusercontent.com/emmanuelatikese/alx-low_level_programming/master/0x18-dynamic_libraries/hk.so
 export LD_PRELOAD=$PWD/hk.so
